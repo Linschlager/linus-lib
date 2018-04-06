44eaf80ac6f0556d7df4560c560e6aca9c0bf942
Linus T. Vettiger <linus_thomas.vettiger@novartis.com>
2018-04-06 15:34:06 +0200

    Added documentation

--- src/toDictionary.js
+++ src/toDictionary.js
@@ -1,9 +1,22 @@
+/**
+ * Tool to parse an array of objects to a dictionary with the same data
+ * ```js
+ * toDictionary([{ key: 'foo', value: 'bar'}]) === { foo: 'bar' }
+ * ```
+ * @param {array} array array containing the information for building the dictionary
+ * @param {string} key name of the key attribute
+ * @param {string} value name of the value attribute
+ */
 export default function toDictionary(array, key = 'name', value = 'value') {
-    if (!array || typeof array === "object" && array.length === undefined) {
-        return array;
+    // Check if array is an actual array
+    if (typeof array === "object" && array.length !== undefined) {
+        return array.reduce((map, next) => ({
+            // return the existing object
+            ...map,
+            // Add the new key-value pair to the dictionary
+            [next[key]]: next[value],
+        }), {});
     }
-    return array.reduce((map, next) => ({
-        ...map,
-        [next[key]]: next[value],
-    }), {});
+    // Return the invalid array parameter
+    return array;
 }
