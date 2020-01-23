a842f4601a318d940cca229e4f4370fe38a08a1f
Vettiger, Linus <linus.vettiger@novartis.com>
2020-01-23 11:57:30 +0100

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env', 'stage-0']
-                }
+                loader: 'babel-loader'
             }
         ]
     },
